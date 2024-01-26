708d862d9bada80bae629abdf23e389384963e16
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-01-26 09:37:09 +0800

    Update n1-diy-part2.sh

--- n1-diy-part2.sh
+++ n1-diy-part2.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
+sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
