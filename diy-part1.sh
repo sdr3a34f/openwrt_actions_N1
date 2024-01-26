72ecad5d302b9d825d5c426dc23ebaa04871f665
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-01-26 09:34:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
+## passwall2
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+
+## amlogic
+git submodule add https://github.com/ophub/luci-app-amlogic.git package/others/luci-app-amlogic
